4a8dd3fcd2e26c83d17ac03af9355c565506368b
HyperBreed2275 <zelenifiver@gmail.com>
2023-01-18 03:18:33 -0800

    finish

--- src/api/mainFunctions.js
+++ src/api/mainFunctions.js
@@ -1,7 +1,7 @@
 exports.dayToMilliSec = (dayValue_d) => {
-    console.log('dayToMillisec log - 1 : ', dayValue_d);
-    // return dayValue_d * 3600 * 24 * 1000;
-    return dayValue_d * 60 * 1000;
+    console.log('dayToMilliSec log - 1 : ', dayValue_d);
+    return dayValue_d * 3600 * 24 * 1000;
+    // return dayValue_d * 60 * 1000;
 }
 
 exports.changeToRealHbarBalance = (balance_c) => {
